6fc199302c4f68ea6ac3b7e0e7086f30fb78b3df
Paul Cho <paul.cho@ttu.edu>
2016-03-07 14:24:23 -0600

    Added functionality to the button "Play Computer"

--- app/src/main/java/team2/shattlebip/MainMenu.java
+++ app/src/main/java/team2/shattlebip/MainMenu.java
@@ -1,11 +1,13 @@
 package team2.shattlebip;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.GridView;
 
 import java.util.ArrayList;
@@ -14,10 +16,30 @@ import java.util.ArrayList;
 /**
  * Created by Paul on 3/7/2016.
  */
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity implements View.OnClickListener{
+
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mm_main);
+        button1 = (Button)findViewById(R.id.button2);
+        button1.setOnClickListener(this);
+    }
+
+    private void button2Click()
+    {
+        startActivity(new Intent("team2.shattlebip.MainActivity"));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.button2:
+                button2Click();
+                break;
+
+        }
     }
 }
