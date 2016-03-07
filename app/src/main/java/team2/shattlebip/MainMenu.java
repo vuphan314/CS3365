54b51c8418ce9ece896e4c3ee5a0eab304133700
Paul Cho <paul.cho@ttu.edu>
2016-03-07 13:54:04 -0600

    Created a Main Menu class and layout

---
+++ app/src/main/java/team2/shattlebip/MainMenu.java
@@ -0,0 +1,23 @@
+package team2.shattlebip;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by Paul on 3/7/2016.
+ */
+public class MainMenu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mm_main);
+    }
+}
