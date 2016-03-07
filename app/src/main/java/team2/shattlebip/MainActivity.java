cb149d3ab57ed5db6a8d667e0b93062c93edd1ca
Vu Phan <vuphan314@gmail.com>
2016-03-07 02:07:03 -0600

    Vu finished button `ARRANGE`
    IT'S WORKINGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG

--- app/src/main/java/team2/shattlebip/MainActivity.java
+++ app/src/main/java/team2/shattlebip/MainActivity.java
@@ -9,13 +9,13 @@ import android.widget.GridView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     GameState gameState;
-    AdapterBoard adapterBoard1, adapterBoard2;
-
-    GridView gridViewBoard1, gridViewBoard2;
     Button buttonArrange;
+    GridView gridViewBoard1, gridViewBoard2;
+    AdapterBoard adapterBoard1, adapterBoard2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +23,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         gameState = new GameState(getString(R.string.game_stage_initialized), -1);
-
+        buttonArrange = (Button) findViewById(R.id.button_arrange);
+        gridViewBoard1 = (GridView) findViewById(R.id.gridViewBoard1);
+        gridViewBoard2 = (GridView) findViewById(R.id.gridViewBoard2);
         adapterBoard1 = new AdapterBoard(this, new ArrayList<BoardCell>());
         adapterBoard2 = new AdapterBoard(this, new ArrayList<BoardCell>());
 
-        gridViewBoard1 = (GridView) findViewById(R.id.gridViewBoard1);
-        gridViewBoard2 = (GridView) findViewById(R.id.gridViewBoard2);
-
-        buttonArrange = (Button) findViewById(R.id.button_arrange);
-
         initializeGame();
-        toastStage();
 
+        arrange();
+    }
+
+    public void initializeGame() {
+        inflateBoard(1);
+        inflateBoard(2);
+        arrange2();
+        toastStage();
+    }
+
+    public void inflateBoard(int playerNum) {
+        getGridViewBoard(playerNum).setAdapter(getAdapterBoard(playerNum));
+        initializeBoard(playerNum);
+    }
+
+    public void initializeBoard(int playerNum) {
+        String boardCellsStatus;
+        for (int i = 0; i < Math.pow(getResources().getInteger(R.integer.board_side_cells_count), 2); i++) {
+            boardCellsStatus = getResources().getString(R.string.board_cell_status_vacant);
+            BoardCell boardCell = new BoardCell(playerNum, boardCellsStatus);
+            getAdapterBoard(playerNum).add(boardCell);
+        }
+    }
+
+    public void arrange2() {
+        Random random = new Random();
+        int cell1 = random.nextInt(4);
+        for (int i = 0; i < 2; i++) {
+            BoardCell boardCell = adapterBoard2.getItem(cell1 + i);
+            boardCell.boardCellStatus = getString(R.string.board_cell_status_occupied);
+        }
+        adapterBoard1.notifyDataSetChanged();
+    }
+
+    public void arrange() {
         buttonArrange.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,21 +87,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void initializeGame() {
-        inflateBoard(1);
-        inflateBoard(2);
-    }
-
-    public void inflateBoard(int playerNum) {
-        getGridViewBoard(playerNum).setAdapter(getAdapterBoard(playerNum));
-        initializeBoard(playerNum);
-    }
-
-    public void initializeBoard(int playerNum) {
-        for (int i = 0; i < Math.pow(R.integer.board_side_cells_count, 2); i++) {
-            BoardCell boardCell = new BoardCell(playerNum, getString(R.string.board_cell_status_vacant));
-            getAdapterBoard(playerNum).add(boardCell);
-        }
+    public void toastStage() {
+        Toast.makeText(this, gameState.gameStage, Toast.LENGTH_SHORT).show();
     }
 
     public GridView getGridViewBoard(int playerNum) {
@@ -85,9 +103,5 @@ public class MainActivity extends AppCompatActivity {
             return adapterBoard1;
         else
             return adapterBoard2;
-    }
-
-    public void toastStage() {
-        Toast.makeText(this, gameState.gameStage, Toast.LENGTH_SHORT).show();
     }
 }
