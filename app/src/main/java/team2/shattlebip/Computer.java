266ac2d7a5f33d979ef63d3c2e6be0e160e96033
twi <twilightnoir@gmail.com>
2016-03-07 17:21:51 -0600

    Changed author name to actual name

--- app/src/main/java/team2/shattlebip/Computer.java
+++ app/src/main/java/team2/shattlebip/Computer.java
@@ -7,6 +7,6 @@ public class Computer extends Player
 {
     public Computer()
     {
-        
+
     }
 }
--- app/src/main/java/team2/shattlebip/Player2.java
+++ app/src/main/java/team2/shattlebip/Player2.java
@@ -7,6 +7,6 @@ public class Player2 extends Player
 {
     public Player2()
     {
-        
+
     }
 }
