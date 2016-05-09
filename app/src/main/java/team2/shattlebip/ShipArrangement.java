72c6a81a9ecc60272bc790106c7730b2de40b6e8
Paul Cho <paul.cho@ttu.edu>
2016-05-09 10:55:12 -0500

    Added documentation, removed redundancy, edited checkArrange()

--- app/src/main/java/team2/shattlebip/ShipArrangement.java
+++ app/src/main/java/team2/shattlebip/ShipArrangement.java
@@ -7,6 +7,8 @@ public class ShipArrangement {
     Cell checkL1, checkL2, checkL3, checkL4, checkL5;
     Cell checkM1, checkM2, checkM3;
 
+
+    //checks to see if there is a valid Large Horizontal arrangement
     public boolean checkArrangeLH(AdapterBoard board) {
         int boardSize = (int) Math.sqrt(board.getCount());
         for (int i = 0; i < board.getCount() - 4; i++) {
@@ -15,7 +17,7 @@ public class ShipArrangement {
                 if (((i + 1) % boardSize == boardSize - 1) ||
                         ((i + 2) % boardSize == boardSize - 1) ||
                         ((i + 3) % boardSize == boardSize - 1)) {
-                    continue;
+                    //loop continues
                 } else {
                     Cell cell2 = board.getItem(i + 1);
                     if (cell2.getStatus() == Cell.Status.OCCUPIED) {
@@ -41,6 +43,7 @@ public class ShipArrangement {
         return false;
     }
 
+    //checks to see if there is a valid Large Vertical arrangement
     public boolean checkArrangeLV(AdapterBoard board) {
         int boardSize = (int) Math.sqrt(board.getCount());
         for (int i = 0; i < (board.getCount() - (boardSize * 4)); i++) {
@@ -69,24 +72,54 @@ public class ShipArrangement {
         return false;
     }
 
+    //checks to see if there is a valid Medium Horizontal arrangement
     public boolean checkArrangeMH(AdapterBoard board) {
         int boardSize = (int) Math.sqrt(board.getCount());
         for (int i = 0; i < board.getCount() - 2; i++) {
             Cell cell = board.getItem(i);
             if (cell.getStatus() == Cell.Status.OCCUPIED) {
-                if (((i + 1) % boardSize == boardSize - 1) ||
-                        ((i + 2) % boardSize == boardSize - 1)) {
-                    continue;
-                } else {
-                    Cell cell2 = board.getItem(i + 1);
+                if((board.getItem(i)!=checkL1)&&(board.getItem(i)!=checkL2)&&(board.getItem(i)!=checkL3)&&(board.getItem(i)!=checkL4)&&(board.getItem(i)!=checkL5)){
+                    if (((i + 1) % boardSize == boardSize - 1) ||
+                            ((i + 2) % boardSize == boardSize - 1)) {
+                        //loop continues
+                    } else {
+                        Cell cell2 = board.getItem(i + 1);
+                        if (cell2.getStatus() == Cell.Status.OCCUPIED) {
+                            Cell cell3 = board.getItem(i + 2);
+                            if (cell3.getStatus() == Cell.Status.OCCUPIED) {
+                                if ((cell3 != checkL1) && (cell3 != checkL2) && (cell3 != checkL3)
+                                        && (cell3 != checkL4) && (cell3 != checkL5)) {
+                                    checkM1 = board.getItem(i);
+                                    checkM2 = board.getItem(i + 1);
+                                    checkM3 = board.getItem(i + 2);
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+
+    //checks to see if there is a valid Medium Vertical arrangement
+    public boolean checkArrangeMV(AdapterBoard board) {
+        int boardSize = (int) Math.sqrt(board.getCount());
+        for (int i = 0; i < (board.getCount() - (boardSize * 2)); i++) {
+            Cell cell = board.getItem(i);
+            if (cell.getStatus() == Cell.Status.OCCUPIED) {
+                if((board.getItem(i)!=checkL1)&&(board.getItem(i)!=checkL2)&&(board.getItem(i)!=checkL3)&&(board.getItem(i)!=checkL4)&&(board.getItem(i)!=checkL5)){
+                    Cell cell2 = board.getItem(i + boardSize);
                     if (cell2.getStatus() == Cell.Status.OCCUPIED) {
-                        Cell cell3 = board.getItem(i + 2);
+                        Cell cell3 = board.getItem(i + boardSize * 2);
                         if (cell3.getStatus() == Cell.Status.OCCUPIED) {
                             if ((cell3 != checkL1) && (cell3 != checkL2) && (cell3 != checkL3)
                                     && (cell3 != checkL4) && (cell3 != checkL5)) {
                                 checkM1 = board.getItem(i);
-                                checkM2 = board.getItem(i + 1);
-                                checkM3 = board.getItem(i + 2);
+                                checkM2 = board.getItem(i + boardSize);
+                                checkM3 = board.getItem(i + boardSize * 2);
                                 return true;
                             }
                         }
@@ -97,21 +130,25 @@ public class ShipArrangement {
         return false;
     }
 
-    public boolean checkArrangeMV(AdapterBoard board) {
+
+    //checks to see if there is a valid Small Horizontal arrangement
+    public boolean checkArrangeSH(AdapterBoard board) {
         int boardSize = (int) Math.sqrt(board.getCount());
-        for (int i = 0; i < (board.getCount() - (boardSize * 2)); i++) {
+        for (int i = 0; i < board.getCount(); i++) {
             Cell cell = board.getItem(i);
             if (cell.getStatus() == Cell.Status.OCCUPIED) {
-                Cell cell2 = board.getItem(i + boardSize);
-                if (cell2.getStatus() == Cell.Status.OCCUPIED) {
-                    Cell cell3 = board.getItem(i + boardSize * 2);
-                    if (cell3.getStatus() == Cell.Status.OCCUPIED) {
-                        if ((cell3 != checkL1) && (cell3 != checkL2) && (cell3 != checkL3)
-                                && (cell3 != checkL4) && (cell3 != checkL5)) {
-                            checkM1 = board.getItem(i);
-                            checkM2 = board.getItem(i + boardSize);
-                            checkM3 = board.getItem(i + boardSize * 2);
-                            return true;
+                if((board.getItem(i)!=checkM1)&&(board.getItem(i)!=checkM2)&&(board.getItem(i)!=checkM3)
+                        &&((board.getItem(i)!=checkL1)&&(board.getItem(i)!=checkL2)&&(board.getItem(i)!=checkL3)&&(board.getItem(i)!=checkL4)&&(board.getItem(i)!=checkL5))){
+                    if ((i + 1) % boardSize == boardSize - 1) {
+                        //loop continues
+                    } else {
+                        Cell cell2 = board.getItem(i + 1);
+                        if (cell2.getStatus() == Cell.Status.OCCUPIED) {
+                            if ((cell2 != checkM1) && (cell2 != checkM2) && (cell2 != checkM3)
+                                    && (cell2 != checkL1) && (cell2 != checkL2) && (cell2 != checkL3)
+                                    && (cell2 != checkL4) && (cell2 != checkL5)) {
+                                return true;
+                            }
                         }
                     }
                 }
@@ -120,39 +157,22 @@ public class ShipArrangement {
         return false;
     }
 
-    public boolean checkArrangeSH(AdapterBoard board) {
+
+    //checks to see if there is a valid Small Vertical arrangement
+    public boolean checkArrangeSV(AdapterBoard board) {
         int boardSize = (int) Math.sqrt(board.getCount());
-        for (int i = 0; i < board.getCount(); i++) {
+        for (int i = 0; i < (board.getCount() - boardSize); i++) {
             Cell cell = board.getItem(i);
             if (cell.getStatus() == Cell.Status.OCCUPIED) {
-                if ((i + 1) % boardSize == boardSize - 1) {
-                    continue;
-                } else {
-                    Cell cell2 = board.getItem(i + 1);
+                if((board.getItem(i)!=checkM1)&&(board.getItem(i)!=checkM2)&&(board.getItem(i)!=checkM3)
+                        &&((board.getItem(i)!=checkL1)&&(board.getItem(i)!=checkL2)&&(board.getItem(i)!=checkL3)&&(board.getItem(i)!=checkL4)&&(board.getItem(i)!=checkL5))){
+                    Cell cell2 = board.getItem(i + boardSize);
                     if (cell2.getStatus() == Cell.Status.OCCUPIED) {
                         if ((cell2 != checkM1) && (cell2 != checkM2) && (cell2 != checkM3)
                                 && (cell2 != checkL1) && (cell2 != checkL2) && (cell2 != checkL3)
                                 && (cell2 != checkL4) && (cell2 != checkL5)) {
                             return true;
                         }
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean checkArrangeSV(AdapterBoard board) {
-        int boardSize = (int) Math.sqrt(board.getCount());
-        for (int i = 0; i < (board.getCount() - boardSize); i++) {
-            Cell cell = board.getItem(i);
-            if (cell.getStatus() == Cell.Status.OCCUPIED) {
-                Cell cell2 = board.getItem(i + boardSize);
-                if (cell2.getStatus() == Cell.Status.OCCUPIED) {
-                    if ((cell2 != checkM1) && (cell2 != checkM2) && (cell2 != checkM3)
-                            && (cell2 != checkL1) && (cell2 != checkL2) && (cell2 != checkL3)
-                            && (cell2 != checkL4) && (cell2 != checkL5)) {
-                        return true;
                     }
                 }
             }
